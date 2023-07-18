96a7550c2c6be3e0b50621c8aa8fac23d7eac05d
janezb10 <janezb10@gmail.com>
2023-07-18 10:50:50 +0200

    ExpenseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import ExpenseList from "./expense-tracker/components/ExpenseList.tsx";
+import { useState } from "react";
+
 function App() {
-  return <>asdf asdf asdf</>;
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "asdf", amount: 10, category: "Utilities" },
+  ]);
+
+  return (
+    <>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((exp) => exp.id !== id))}
+      ></ExpenseList>
+    </>
+  );
 }
 
 export default App;
--- src/expense-tracker/components/ExpenseList.tsx
+++ src/expense-tracker/components/ExpenseList.tsx
@@ -2,13 +2,17 @@ interface Expense {
   id: number;
   description: string;
   amount: number;
+  category: string;
 }
 
 interface Props {
-  expenses: Expense;
+  expenses: Expense[];
+  onDelete: (id: number) => void;
 }
 
-function ExpenseList() {
+function ExpenseList({ expenses, onDelete }: Props) {
+  if (expenses.length === 0) return null;
+
   return (
     <table className="table table-bordered">
       <thead>
@@ -19,7 +23,35 @@ function ExpenseList() {
           <th></th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        {expenses.map((expense) => {
+          return (
+            <tr key={expense.id}>
+              <td>{expense.description}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.category}</td>
+              <td>
+                <button
+                  onClick={() => onDelete(expense.id)}
+                  className="btn btn-outline-danger"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>
+            €{expenses.reduce((acc, exp) => exp.amount + acc, 0).toFixed(2)}
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
     </table>
   );
 }
